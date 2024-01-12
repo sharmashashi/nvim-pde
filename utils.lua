e4831867e20971535a38410f10be6e8249485a75
Shashi Sharma <sharmashashi940@gmail.com>
2024-01-12 09:31:37 +0545

    add dart format

--- utils.lua
+++ utils.lua
@@ -5,3 +5,13 @@ vim.api.nvim_set_keymap('n', '<leader>w', '<C-w>w', {
 })
 -- command mode on esc on shell
 vim.cmd([[tnoremap <Esc> <C-\><C-n>]])
+
+-- command to save, format and re load the current dart file
+function FormatCurrentFile()
+    vim.cmd(":!dart format %")
+    vim.fn.feedkeys("<CR>", "n")
+end
+
+-- Map <Leader>df to call the format function
+vim.api.nvim_set_keymap('n', '<Leader>df', [[:lua FormatCurrentFile()<CR>]], { noremap = true, silent = true })
+
